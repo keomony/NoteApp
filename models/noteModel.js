cab44949627a846e8a317015601657b019768e61
James <jamesdix@cwgsy.net>
2017-01-03 18:43:55 +0000

    starting again

--- models/noteModel.js
+++ models/noteModel.js
@@ -1,13 +1,1 @@
-(function(exports) {
 
-  var Note = function(text){
-    this.text = text
-  };
-
-  Note.prototype.view = function() {
-    return this.text;
-  };
-
-  exports.Note = Note;
-
-})(this);
