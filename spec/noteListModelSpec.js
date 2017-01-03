d93f169b93ab730596c3770dab916605a2ba4e95
James <jamesdix@cwgsy.net>
2017-01-03 21:29:10 +0000

    spec file refactors

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -1,10 +1,15 @@
-function noteListModelSpec() {
+function noteListInstatiatedWithArray() {
   list = new NoteList();
 
   assert.isTrue(Array.isArray(list.noteModels()));
+}
 
+function noteListCanAddNotesToArray() {
+  list = new NoteList();
   list.addNote("My first note!");
+
   assert.isTrue(list.noteModels()[0].view() === "My first note!");
 }
 
-noteListModelSpec();
+noteListInstatiatedWithArray();
+noteListCanAddNotesToArray();
