b7ef526898f8f0eb003efa16a0f0b3645bea8362
Chris Cooper <cjcooper252@gmail.com>
2017-01-04 10:03:46 +0000

    handles note list with no notes

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -5,7 +5,13 @@
   noteList.storeNote("Favourite food: Amok");
   noteList.storeNote("Favourite food: Green Fish Curry");
   var noteListView = new NoteListView(noteList);
-  console.log(noteListView.viewHTML());
-  assert.isTrue(noteListView.viewHTML().includes("<li><div>Favourite food: Amok</div></li>"), "should return a string of HTML that represents the note list model");
+  assert.isTrue(noteListView.viewHTML().includes("<ul><li><div>Favourite food: Amok</div></li><li><div>Favourite food: Green Fish Curry</div></li></ul>"),
+  "should return a string of HTML that represents the note list model");
+})();
 
+(function testNoteListView(){
+  var noteList = new NoteList();
+  var noteListView = new NoteListView(noteList);
+  assert.isTrue(noteListView.viewHTML().includes("<ul></ul>"),
+  "should return a string of HTML that represents the empty note list model");
 })();
