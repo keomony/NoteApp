fe3f79200f480b2e9c823f250ce16db9a49b6daa
Chris Cooper <cjcooper252@gmail.com>
2017-01-04 15:37:06 +0000

    notes have id

--- spec/noteListModelSpec.js
+++ spec/noteListModelSpec.js
@@ -16,3 +16,13 @@
   assert.isTrue(allNotes.length == 2, "should show all note objects");
   assert.isTrue(allNotes[0] instanceof Note, "should be an note object");
 })();
+
+(function testNoteIdInOrder() {
+  var noteList = new NoteList();
+  noteList.storeNote("hi");
+  noteList.storeNote("hello");
+  var allNotes = noteList.showAllNotes();
+  assert.isTrue(allNotes[0].id === 0, "should give first note an id of 0");
+  assert.isTrue(allNotes[1].id === 1, "should give second note an id of 1");
+
+})();
--- src/noteListModel.js
+++ src/noteListModel.js
@@ -4,11 +4,13 @@
 
   var NoteList = function(){
     this.notes = new Array;
+    this.count = 0;
   };
 
   NoteList.prototype.storeNote = function(inputText){
-    var note = new Note(inputText)
+    var note = new Note(inputText,this.count)
     this.notes.push(note);
+    this.count ++;
   };
 
   NoteList.prototype.showAllNotes = function(){
