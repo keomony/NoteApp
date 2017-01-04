2659ded57563de29e143a26dd4df0a9b8af46155
James <jamesdix@cwgsy.net>
2017-01-04 15:02:48 +0000

    mocks and doubles complete

--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -1,6 +1,6 @@
 function noteControllerInstantiated(){
-  var noteList = new NoteList()
-  var controller = new NoteController(noteList)
+  var noteList = new NoteList();
+  var controller = new NoteController(noteList);
   assert.isTrue(controller.hasOwnProperty('noteListView'));
 }
 
@@ -9,14 +9,14 @@ noteControllerInstantiated();
 function validateInnerHTML(){
 
   //mock up a new note list, that responds to addNote() method by doing nothing
-  function noteListDouble() {};
+  function noteListDouble() {}
   noteListDouble.prototype.addNote = function(){};
 
   //mock up a new note list view
-  function noteListViewDouble(noteListDouble){};
+  function noteListViewDouble(noteListDouble){}
   noteListViewDouble.prototype.convert = function(){
-    return "<ul><div><li>Test Note</li></div></ul>"
-  }
+    return "<ul><div><li>Test Note</li></div></ul>";
+  };
 
 
   //creating all the stuff we need
@@ -25,9 +25,9 @@ function validateInnerHTML(){
 
   //override line 5 in controller code
   controller.noteListView = new noteListViewDouble();
-  controller.convertToHTML()
+  controller.convertToHTML();
 
-  assert.isTrue(element.innerHTML === "<ul><div><li>Test Note</li></div></ul>")
+  assert.isTrue(document.getElementById("app").innerHTML === "<ul><div><li>Test Note</li></div></ul>");
 
 }
 
