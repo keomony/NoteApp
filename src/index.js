8120c4e070cb24fd1215033996590ef692289a13
keomony khun <keomony@gmail.com>
2017-01-05 22:49:02 +0000

    multiple content pages in a frontend, single page app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@
 var noteList = new NoteList();
 var noteController = new NoteController(noteList);
 noteController.addNote("Favourite drink: seltzer");
+noteController.addNote("Shopping list: Amok, Milk, Eggs, Bread");
+noteController.addNote("Favourite food: Green Fish Curry");
 noteController.getHTML();
+
+makeUrlChangeShowNoteForCurrentPage();
+
+function makeUrlChangeShowNoteForCurrentPage() {
+  window.addEventListener("hashchange", showNoteForCurrentPage);
+};
+
+function showNoteForCurrentPage() {
+  showNote(getNoteFromUrl(window.location));
+};
+
+function getNoteFromUrl(location) {
+  return location.hash.split("#")[1];
+};
+
+function showNote(noteId) {
+  var note = noteList.getNoteById(noteId);
+  var singleNoteView = new SingleNoteView(note);
+  document
+    .getElementById("viewNote")
+    .innerHTML = singleNoteView.viewHTML();
+};
